51625e5754ee5c214445439bc8e0b65c869027ca
shree026 <026shreyasharma@gmail.com>
2019-10-18 00:22:21 +0530

    web-page

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>PHP-EXAMPLE</title>
+</head>
+<body>
+<h1> JUST CHECKING IN !! </h1>
+<h1> HOLA ~~ AMIGO !! </h1>
+<img src="php.jpg">
+<?php
+echo   "Hello world";
+echo   "time to deploy something  ";
+echo  phpinfo();
+?>
+
+</body>
+</html>
